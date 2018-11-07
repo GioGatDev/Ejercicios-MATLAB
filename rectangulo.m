e29bb138da11f93d4b8ac2eb71e70e99e3675e89
Gatdev <giovannigatfut@gmail.com>
2018-11-07 11:30:34 -0600

    Triangulo y rectangulo con asteriscos

---
+++ rectangulo.m
@@ -0,0 +1,9 @@
+% Codigo para imprimir un rectangulo
+fprintf('\nrectangulo\n\n')
+for k=1:7
+   for l=1:6
+      fprintf('*')
+   end;
+   
+   fprintf('\n')
+end
---
+++ triangulo.m
@@ -0,0 +1,9 @@
+% Codigo para imprimir un triangulo
+fprintf('\ntriangulo\n\n')
+for k=1:7
+   for l=1:k
+      fprintf('*')
+   end;
+   fprintf('\n')
+   
+end
