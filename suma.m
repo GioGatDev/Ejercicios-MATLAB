a139330582a3432b4cbfbe8de4723bcedc75ca5c
Gatdev <giovannigatfut@gmail.com>
2018-11-07 11:28:36 -0600

    Suma y resta de matrices

---
+++ suma.m
@@ -0,0 +1,15 @@
+function C = suma(A, B)
+  [N, M] = size(A);
+  [R, S] = size(B);
+  
+  if N~=R || M~=S
+      fprintf('Las dimensiones tienen que ser iguales\n');
+      return;
+  end;
+  C = zeros(N, M);
+  for n=1:N
+      for m = 1:M
+          C(n,m) = A(n,m)+B(n,m);
+      end;
+  end
+end
